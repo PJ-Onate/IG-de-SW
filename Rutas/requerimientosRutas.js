4631d093cfcaee168fb4ee4797a361d961611d98
PJ-Onate <114963730+PJ-Onate@users.noreply.github.com>
2022-12-03 23:30:37 -0300

    cambio a post hacerBoletas

--- Rutas/requerimientosRutas.js
+++ Rutas/requerimientosRutas.js
@@ -10,7 +10,7 @@ api.post('/sent-email', emailControlador.email);
 api.put('/pagoDeuda', requerimientoPago.pagoDeudaTotal);
 api.get('/pagoGasto', requerimientoPago.pagarGasto);
 api.get('abonarDeuda', requerimientoPago.abonar);
-api.get('/ingresarBoleta', ingresarBoleta.crearBoleta);
-api.get('/crearGastoComun', ingresarBoleta.crearGastoComun);
+api.post('/ingresarBoleta', ingresarBoleta.crearBoleta);
+api.post('/crearGastoComun', ingresarBoleta.crearGastoComun);
 
 module.exports=api;
